1c8c8d9977348709355410780672dba3e37420b8
Beata Karolczak <karolczak.beata@gmail.com>
2018-05-02 15:18:05 +0200

    Finish PlayerList component and tests

--- src/components/PlayersList/PlayersList.test.js
+++ src/components/PlayersList/PlayersList.test.js
@@ -1,6 +1,7 @@
 import PlayersList from './PlayersList';
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { shallow } from 'enzyme';
+import Player from '../Player/Player';
 
 it('renders without crashing', () => {
   shallow(<PlayersList players={[]} />);
@@ -17,13 +18,35 @@ it('renders correct number of players', () => {
         score: 0
     }
 ]
-const playerComponent = mount(<PlayersList players={players} />);
+const playerComponent = shallow(<PlayersList players={players} />);
 
 console.log(playerComponent.debug());
 
-const expectedPlayersNumber = playerComponent.find('li').length;
+const expectedPlayersNumber = playerComponent.find(Player).length;
 
 expect(expectedPlayersNumber).toEqual(2);
 
 });
 
+it('should call onScoreUpdate with 1 when minus button is clicked', () => {
+  const players = [
+    {
+        name: 'Kunegunda',
+        score: 5
+    },
+    {
+        name: 'Antoś',
+        score: 0
+    }
+  ]
+  const mockedOnScoreUpdate = jest.fn();
+  const playerComponent = shallow(<PlayersList players={players} onScoreUpdate={mockedOnScoreUpdate} />);
+
+  const lastPlayer = playerComponent.find(Player).last();
+
+  const onPlayerScoreChange = lastPlayer.prop('onPlayerScoreChange');
+  onPlayerScoreChange(5);
+
+  expect(mockedOnScoreUpdate).toBeCalledWith(1, 5);
+
+});
